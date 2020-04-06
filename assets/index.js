d95b4c58de93ca55eb14d3dba107b9b577904ecf
Muhammad Fadhil <33900154+devfdL@users.noreply.github.com>
2020-04-06 18:34:02 +0700

    Add files via upload

--- assets/index.js
+++ assets/index.js
@@ -1,17 +1,33 @@
 var testCount = 0;
 var out = document.getElementById("output");
 var x = document.getElementById("myVideo");
+var y = document.getElementById("myVideo1");
 
 function go(){
   testCount++;
   switch (testCount) {
     case 1:
         document.getElementById("btn-a").style.display = "block";
+        document.getElementById("ads").style.display = "block";
         document.getElementById("myVideo").style.display = "block";
         x.autoplay = true;
         x.load();
         setTimeout(function() {
-            $('#myVideo').fadeOut('fast');
+          $('#myVideo').fadeOut('fast');
+        }, 20000); // <-- time in milliseconds
+        
+        setTimeout(function() {
+          $('#ads').fadeOut('fast');
+        }, 20000); // <-- time in milliseconds
+      break;
+
+      case 2:
+        document.getElementById("btn-b").style.display = "block";
+        document.getElementById("myVideo1").style.display = "block";
+        y.autoplay = true;
+        y.load();
+        setTimeout(function() {
+            $('#myVideo1').fadeOut('fast');
         }, 10000); // <-- time in milliseconds
       break;
 
